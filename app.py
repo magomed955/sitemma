fece05a70c59888d3db4611f4aeb1e78f1f5677c
Magomed Mutsulkhanov <mmutsulk@k2r1p1.42mulhouse.fr>
2026-05-20 13:39:45 +0200

    push

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = "ironcage_secret_2025"
+@app.after_request
+def no_cache(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 if not os.path.exists("static/img"):
     os.makedirs("static/img")
 DB = "database.db"
